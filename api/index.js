00fe05bcbf9b689400e1534a23feb47a69edd4d3
sunayanativale <sunayana@chainworks.io>
2024-05-20 20:17:35 +0530

    database connection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log(`connected to DB`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.listen(3000, () => {
   console.log(`Listening on port 3000`);
 });
